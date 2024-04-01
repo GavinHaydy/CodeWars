e5368c6804531236582e3a4ea4b6e7cabecb6fd8
GavinHaydy <13558622779@163.com>
2024-04-01 11:27:07 +0800

    init

---
+++ create_code_file.sh
@@ -0,0 +1,41 @@
+if [ $# != 2 ];
+then
+    echo "参数个数错误"
+    exit 1
+fi
+
+array=(1
+2
+3
+4
+5
+6
+7
+8
+)
+
+lau=(
+    .ts
+    .go
+    .java
+    .js
+    .py
+    .md
+)
+
+found=false
+for element in "${array[@]}"; do
+    if [ "$element" == "$1" ]; then
+        found=true
+        break
+    fi
+done
+
+if [ "$found" = true ]; then
+    cd $1ku && mkdir $2
+    for filename in "${lau[@]}";do
+        touch "$2/$2$filename"
+    done
+else
+    echo "$1ku文件夹不存在"
+fi
