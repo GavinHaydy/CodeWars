4f47a1d1728d7ea8f00905230c200769e226b820
GavinHaydy <13558622779@163.com>
2024-04-05 14:10:22 +0800

    perf: 删除测试代码

--- test/test.go
+++ test/test.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func FirstNonRepeating(str string) string {
-	//your code here
-	low := strings.ToLower(str)
-	for k, _ := range low {
-		// fmt.Println(str[k : k+1])
-		if strings.Count(low, low[k:k+1]) == 1 {
-			fmt.Println(strings.Count(low, str[k:k+1]))
-			return str[k : k+1]
-		}
-
-	}
-	return ""
-}
 func main() {
-	fmt.Println(FirstNonRepeating("stress"))
-	fmt.Println(FirstNonRepeating("sTreSS"))
 }
